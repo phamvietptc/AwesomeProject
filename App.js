b4b899c61d617f3d7238efd71979dbcc2271232d
phamvietptc <phamvietptc@gmail.com>
2022-02-28 16:18:19 +0700

    28/2/2022

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from "react-native";
+import Constant from "react-native";
+import { StatusBar } from "react-native";
 
 export default function LoginAcc() {
   return (
     <SafeAreaView style={style.container}>
+      <StatusBar style="light" />
       <View style={style.content}>
         <View style={style.textWrapper}>
           <Text style={style.hiText}>Xin Chào!</Text>
@@ -11,6 +14,24 @@ export default function LoginAcc() {
           <Text style={style.userText}>0000000000</Text>
         </View>
       </View>
+
+      <View style={style.form}>
+        <TextInput style={style.inputPassword} />
+
+        <TouchableOpacity style={style.buttonLogin}>
+          <Text style={style.buttonLoginText}>ĐĂNG NHẬP</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.buttonAction}>
+          <Text style={style.buttonActionText}>QUÊN MẬT KHẨU</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={style.buttonAction}>
+          <Text style={style.buttonActionText}>THOÁT TÀI KHOẢN</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={style.action}></View>
     </SafeAreaView>
   );
 }
@@ -43,4 +64,23 @@ const style= StyleSheet.create({
     fontSize: 16,
     lineHeight: 30,
   },
+  inputPassword: {
+    height: 60,
+    backgroundColor: "#fff",
+  },
+  buttonLogin: {
+    backgroundColor: "red",
+    height: 50,
+    marginTop: 15,
+    borderRadius: 25,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonLoginText: {
+    ...TEXT
+  },
+  action: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  }
 });
